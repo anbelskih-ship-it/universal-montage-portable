9c848d97f330aa7df292c090fd506ff896adcb40
anbelskih-ship-it <274518555+anbelskih-ship-it@users.noreply.github.com>
2026-05-26 21:02:50 +0300

    Add Windows app-mode launcher package

--- Start-Windows.bat
+++ Start-Windows.bat
@@ -1,3 +1,30 @@
 @echo off
 setlocal
-start "" "%~dp0start.html"
+set "APP=%~dp0start.html"
+set "URL=file:///%APP:\=/%"
+
+set "EDGE=%ProgramFiles(x86)%\Microsoft\Edge\Application\msedge.exe"
+if exist "%EDGE%" (
+  start "" "%EDGE%" --app="%URL%" --window-size=1280,900
+  exit /b
+)
+
+set "EDGE=%ProgramFiles%\Microsoft\Edge\Application\msedge.exe"
+if exist "%EDGE%" (
+  start "" "%EDGE%" --app="%URL%" --window-size=1280,900
+  exit /b
+)
+
+set "CHROME=%ProgramFiles%\Google\Chrome\Application\chrome.exe"
+if exist "%CHROME%" (
+  start "" "%CHROME%" --app="%URL%" --window-size=1280,900
+  exit /b
+)
+
+set "CHROME=%ProgramFiles(x86)%\Google\Chrome\Application\chrome.exe"
+if exist "%CHROME%" (
+  start "" "%CHROME%" --app="%URL%" --window-size=1280,900
+  exit /b
+)
+
+start "" "%APP%"
